8f76ffb749a53ab46a09636269c69dc0214cb9f5
shat700 <60496195+shat700@users.noreply.github.com>
2020-12-13 14:20:10 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -72,8 +72,8 @@ def main():
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
-    #os.makedirs('./outputs',exist_ok=True)
-    #joblib.dump(value=model,filename='./outputs/model.joblib')
+    os.makedirs("outputs",exist_ok=True)
+    joblib.dump(value=model,filename="./outputs/model.joblib")
 
 if __name__ == '__main__':
     main()
